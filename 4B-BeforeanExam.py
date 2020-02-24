5dc19287b5274cc862662a1a8a58c3261d64258c
ByMykel <38622893+ByMykel@users.noreply.github.com>
2020-02-24 16:59:45 +0100

    Create 4B-BeforeanExam.py

---
+++ 4B-BeforeanExam.py
@@ -0,0 +1,18 @@
+d, sumTime = map(int, input().split())
+minTimel = []
+maxTimel = []
+ 
+for i in range(d):
+    minTime, maxTime = map(int, input().split())
+    minTimel.append(minTime)
+    maxTimel.append(maxTime)
+ 
+if d < 1 or d > 30 or sumTime < 0 or sumTime > 240 or sum(maxTimel) < sumTime or minTime > sumTime or sum(minTimel) > sumTime:
+    print("NO")
+else:
+    print("YES")
+    
+    for i in range(d):
+        time = min(minTimel[i] + sumTime - sum(minTimel), maxTimel[i])
+        sumTime -= (time - minTimel[i]);
+        print(str(time), end =" ")
