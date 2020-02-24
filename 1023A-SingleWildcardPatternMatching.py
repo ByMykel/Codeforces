c5b7207f027500519874bd25988aa804a4ca3556
ByMykel <38622893+ByMykel@users.noreply.github.com>
2020-02-24 16:56:11 +0100

    Create 1023A-SingleWildcardPatternMatching.py

---
+++ 1023A-SingleWildcardPatternMatching.py
@@ -0,0 +1,24 @@
+n, m = map(int, input().split())
+s = input()
+t = input()
+ 
+ 
+if n > m + 1:
+    print("NO")
+else:
+    if "*" in s and len(s) > 1:
+        s = s.split("*")
+ 
+        if s[0] == t[:len(s[0])] and s[1] == t[-len(s[1]):]:
+            print("YES")
+        elif s[1] == '' and s[0] == t[:len(s[0])]:
+            print("YES")
+        else:
+            print("NO")
+    else:
+        if s == t:
+            print("YES")
+        elif len(s) == 1 and "*" in s:
+            print("YES")
+        else:
+            print("NO")
