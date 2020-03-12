23207c028378ad8b726cadeeb56850ae57f95881
ByMykel <38622893+ByMykel@users.noreply.github.com>
2020-03-12 21:29:03 +0100

    Update 785A-AntonandPolyhedrons.py

--- 785A-AntonandPolyhedrons.py
+++ 785A-AntonandPolyhedrons.py
@@ -1,12 +1,6 @@
 #https://codeforces.com/problemset/problem/785/A
 
-figures = {
-    "Tetrahedron" : 4, 
-    "Cube" : 6,
-    "Octahedron" : 8,
-    "Dodecahedron" : 12,
-    "Icosahedron" : 20
-}
+figures = {"Tetrahedron" : 4, "Cube" : 6,"Octahedron" : 8, "Dodecahedron" : 12, "Icosahedron" : 20}
 n = int(input())
 solution = 0
 for i in range(n):
