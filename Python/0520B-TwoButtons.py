9ab9e0b76fbf1240f3fa6074a4f4ed804c88143a
ByMykel <38622893+ByMykel@users.noreply.github.com>
2020-03-13 21:10:05 +0100

    Update 0520B-TwoButtons.py

--- Python/0520B-TwoButtons.py
+++ Python/0520B-TwoButtons.py
@@ -3,15 +3,11 @@
 n, m = map(int, input().split())
 times = 0
 while n != m:
-    if n > m:
+    if n > m or m % 2 != 0:
         m += 1
         times += 1
     else:
         if m % 2 == 0:
             m /= 2
             times += 1
-        else:
-            m += 1
-            m /= 2
-            times += 2
 print(times)
