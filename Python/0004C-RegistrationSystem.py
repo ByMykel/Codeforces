725e5d0e7fbdaf08756e4d7a2a684424ded36f26
ByMykel <38622893+ByMykel@users.noreply.github.com>
2020-03-14 13:09:16 +0100

    Update 0004C-RegistrationSystem.py

--- Python/0004C-RegistrationSystem.py
+++ Python/0004C-RegistrationSystem.py
@@ -1,14 +1,16 @@
 #https://codeforces.com/contest/4/problem/C
 
-n = int(input())
-words = {}
-solution = [0] * n
-for i in range(n):
-    word = input()
-    if word in words:
-        solution[i] = f"{word}{words[word]}"
-        words[word] += 1
-    else:
-        solution[i] = "OK"
-        words[word] = 1
-print("\n".join(solution))
+def main():
+    n = int(input())
+    words = {}
+    solution = [0] * n
+    for i in range(n):
+        word = input()
+        if word in words:
+            solution[i] = f"{word}{words[word]}"
+            words[word] += 1
+        else:
+            solution[i] = "OK"
+            words[word] = 1
+    print("\n".join(solution))
+main()
