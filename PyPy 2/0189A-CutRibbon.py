aefe0b33f56ce0ccbfdd7630cc209a5366f5bf70
ByMykel <bymykel5@gmail.com>
2020-03-14 20:16:15 +0100

    Create 0189A-CutRibbon.py

---
+++ PyPy 2/0189A-CutRibbon.py
@@ -0,0 +1,10 @@
+n, a, b, c = map(int, raw_input().split())
+solution = 0
+for i in xrange(n+1):
+    for j in xrange(n+1):
+        z = (n - a*i - b*j) / c
+        if z >= 0 and z % 1 == 0:
+            if n == (a*i + b*j + c*z):
+                if int(i+j+z) > solution:
+                    solution = int(i+j+z)
+print(solution)
