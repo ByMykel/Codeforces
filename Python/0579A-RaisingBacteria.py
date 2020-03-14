58851b5ef2c7028f204a04fe29c8e8ad37f8e448
ByMykel <38622893+ByMykel@users.noreply.github.com>
2020-03-14 14:57:35 +0100

    Create 0579A-RaisingBacteria.py

---
+++ Python/0579A-RaisingBacteria.py
@@ -0,0 +1,17 @@
+#https://codeforces.com/problemset/problem/579/A
+
+import math
+n = int(input())
+if n == 1:
+    print(1)
+else:
+    start = int(math.log2(n))
+    solution = 0
+    for i in range(start, -1, -1):
+        bacteria  = 2**i
+        if n == 0:
+            break
+        elif bacteria  <= n:
+            n -= bacteria 
+            solution += 1
+    print(solution)
