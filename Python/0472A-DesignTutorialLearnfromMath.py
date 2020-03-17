a277f754338687d5da648ce5ab17298e36772f89
ByMykel <38622893+ByMykel@users.noreply.github.com>
2020-03-17 20:06:51 +0100

    Create 0472A-DesignTutorialLearnfromMath.py

---
+++ Python/0472A-DesignTutorialLearnfromMath.py
@@ -0,0 +1,10 @@
+#https://codeforces.com/problemset/problem/472/A
+
+n = int(input())
+if n % 2 == 0:
+    if n//2 % 2 == 0:
+        print(n//2, n//2)
+    else:
+        print(4, n-4)
+else:
+    print(9, n-9)
