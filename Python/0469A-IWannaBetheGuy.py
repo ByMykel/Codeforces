0b8c14c0711900a316daaa36a7cc363d354e2af2
ByMykel <38622893+ByMykel@users.noreply.github.com>
2020-03-17 15:58:57 +0100

    Create 0469A-IWannaBetheGuy.py

---
+++ Python/0469A-IWannaBetheGuy.py
@@ -0,0 +1,10 @@
+#https://codeforces.com/problemset/problem/469/A
+
+n = int(input())
+x = list(map(int, input().split()))
+y = list(map(int, input().split()))
+levels = set(x[1:] + y[1:])
+if len(levels) == n:
+    print("I become the guy.")
+else:
+    print("Oh, my keyboard!")
