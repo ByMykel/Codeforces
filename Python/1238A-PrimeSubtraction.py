c0e624841562b86c4620e94ce77ffe2ec53eb307
ByMykel <38622893+ByMykel@users.noreply.github.com>
2020-03-22 18:07:58 +0100

    Create 1238A-PrimeSubtraction.py

---
+++ Python/1238A-PrimeSubtraction.py
@@ -0,0 +1,9 @@
+#https://codeforces.com/problemset/problem/1238/A
+
+t = int(input())
+for i in range(t):
+    x, y = map(int, input().split())
+    if x - y > 1:
+        print("YES")
+    else:
+        print("NO")
