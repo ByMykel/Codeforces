5d43a36cc6686ed17d7821e0c63fd7e5257f607e
ByMykel <38622893+ByMykel@users.noreply.github.com>
2020-03-30 00:45:43 +0200

    Create 0931A-FriendsMeeting.py

---
+++ Python/0931A-FriendsMeeting.py
@@ -0,0 +1,11 @@
+#https://codeforces.com/problemset/problem/931/A
+
+a = int(input())
+b = int(input())
+d = abs(a - b)
+n = d // 2
+if d % 2 == 0:
+    solution = n * ((n+1) / 2) * 2
+else:
+    solution = n * ((n+1) / 2) + (n+1) * ((n+2) / 2)
+print(int(solution))
