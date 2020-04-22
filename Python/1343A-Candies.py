a353afcf44d251a0f1056afdc52ee21026dd9f6e
ByMykel <38622893+ByMykel@users.noreply.github.com>
2020-04-22 22:28:16 +0200

    Update 1343A-Candies.py

--- Python/1343A-Candies.py
+++ Python/1343A-Candies.py
@@ -5,5 +5,5 @@ for _ in range(t):
     n = int(input())
     for i in range(2, 30):
         if n % ((1 << i) - 1) == 0:
-            print(n // (2**i - 1))
+            print(n // ((1 << i) - 1))
             break
