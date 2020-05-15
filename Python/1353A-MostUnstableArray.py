73764d9e4204e35bb3837d1aba1b559fbcd32d5d
ByMykel <38622893+ByMykel@users.noreply.github.com>
2020-05-15 16:27:20 +0200

    Create 1353A-MostUnstableArray.py

---
+++ Python/1353A-MostUnstableArray.py
@@ -0,0 +1,11 @@
+#https://codeforces.com/problemset/problem/1353/A
+
+t = int(input())
+for _ in range(t):
+    n, m = map(int, input().split())
+    if n == 1:
+        print(0)
+    elif n == 2:
+        print(m)
+    else:
+        print(m*2)
