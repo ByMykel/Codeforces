d15cdd84923a5e88138a3c3a22aefb4679ade06b
ByMykel <38622893+ByMykel@users.noreply.github.com>
2020-05-16 22:42:57 +0200

    Create 0448A-Rewards.py

---
+++ Python/0448A-Rewards.py
@@ -0,0 +1,10 @@
+#https://codeforces.com/contest/448/problem/A
+
+import math
+a = sum(list(map(int, input().split())))
+b = sum(list(map(int, input().split())))
+n = int(input())
+if math.ceil(a/5) + math.ceil(b/10) > n:
+    print("NO")
+else:
+    print("YES")
