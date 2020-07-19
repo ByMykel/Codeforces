56753f8df4b60031bd4e46c0e8609ed633ccf633
ByMykel <38622893+ByMykel@users.noreply.github.com>
2020-07-19 21:04:09 +0200

    Update 1367C-SocialDistance.py

--- Python/1367C-SocialDistance.py
+++ Python/1367C-SocialDistance.py
@@ -4,20 +4,8 @@ t = int(input())
 for _ in range(t):
     n, k = map(int, input().split())
     s = input()
-    if n == k:
-        if s.count("1") != 0:
-            print(0)
-        else:
-            print(1)
-    else:
-        output = 0
-        i = 0
-        while i < n:
-            if s[i] == "1":
-                i += k
-            else:
-                if "1" not in s[i+1:i+k+1]:
-                    output += 1
-                    i += k
-            i += 1
-        print(output)
+    s = ("0"*k + s + "0"*k).split("1")
+    output = 0
+    for i in s:
+        output += (len(i)-k) // (k+1)
+    print(output)
