a81ecaffca92355007208bdc3686ba2eeaf54fc1
ByMykel <38622893+ByMykel@users.noreply.github.com>
2020-07-22 17:16:22 +0200

    Update 1263A-SweetProblem.py

--- Python/1263A-SweetProblem.py
+++ Python/1263A-SweetProblem.py
@@ -4,14 +4,8 @@ t = int(input())
 for _ in range(t):
     r, g, b = sorted(map(int, input().split()))
     ans = 0
-    if b - g >= r:
-        g += r
-        ans = g
-    elif b - g < r:
-        tmp = b - g
-        g += tmp
-        r -= tmp
-        ans = g
-        if r > 1:
-            ans += r//2
+    if r + g <= b:
+        ans = r + g
+    else:
+        ans = (r+g+b) // 2
     print(ans)
