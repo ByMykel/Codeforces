e24d7d12a7f588813e341a474808c4c2dfd5051d
ByMykel <38622893+ByMykel@users.noreply.github.com>
2020-07-27 16:56:44 +0200

    Create 0987B-HighSchoolBecomeHuman.py

---
+++ Python/0987B-HighSchoolBecomeHuman.py
@@ -0,0 +1,12 @@
+#https://codeforces.com/problemset/problem/987/B
+
+import math
+x, y = map(int, input().split())
+a = y * math.log(x)
+b = x * math.log(y)
+if a > b:
+    print(">")
+elif a < b:
+    print("<")
+else:
+    print("=")
