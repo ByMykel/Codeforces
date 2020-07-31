9a704e07b66a84650dad0cfde991a494b7874f43
ByMykel <38622893+ByMykel@users.noreply.github.com>
2020-07-31 12:18:55 +0200

    Create 1206B-MakeProductEqualOne.py

---
+++ Python/1206B-MakeProductEqualOne.py
@@ -0,0 +1,22 @@
+#https://codeforces.com/problemset/problem/1206/B
+
+n = int(input())
+a = sorted(list(map(int, input().split())))
+ans = 0
+neg = 0
+zer = 0
+for i in a:
+    if i > 0:
+        if i != 1:
+            ans += i - 1
+    elif i < 0:
+        if i != -1:
+            ans += abs(i + 1)
+        neg += 1
+    else:
+        ans += 1
+        zer += 1
+if neg % 2 == 1 and zer == 0:
+    print(ans + 2)
+else:
+    print(ans)
