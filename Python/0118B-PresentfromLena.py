e57c2593b12c3e18728eeae0f5975ed3942f5d07
ByMykel <38622893+ByMykel@users.noreply.github.com>
2020-08-08 20:32:21 +0200

    Create 0118B-PresentfromLena.py

---
+++ Python/0118B-PresentfromLena.py
@@ -0,0 +1,14 @@
+#https://codeforces.com/problemset/problem/118/B
+
+n = int(input())
+count = 1
+space = n*2
+k = 1
+for i in range(2*n+1):
+    numbers = [str(i) for i in range(count)]
+    numbers += [str(i) for i in range(count - 2, -1, -1)]
+    print(space * " " + " ".join(numbers))
+    if count == n+1:
+        k = -k
+    count += k
+    space -= k*2
