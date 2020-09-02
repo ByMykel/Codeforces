e09d25cf215c6ac3b31c28e715feae232912b04c
ByMykel <38622893+ByMykel@users.noreply.github.com>
2020-09-02 16:30:51 +0200

    Update template.py

--- template.py
+++ template.py
@@ -2,6 +2,6 @@ import sys
 input = sys.stdin.readline
 ins = lambda: input().rstrip()
 ini = lambda: int(input().rstrip())
-inm = lambda: map(int, input().split())
+inm = lambda: map(int, input().rstrip().split())
 inl = lambda: list(map(int, input().split()))
-out = lambda x: print('\n'.join(map(str, x)))
+out = lambda x, s='\n': print(s.join(map(str, x)))
