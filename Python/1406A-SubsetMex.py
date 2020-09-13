d735fcc60657187d2584b48382ed17eb48bdd74c
ByMykel <38622893+ByMykel@users.noreply.github.com>
2020-09-13 22:10:51 +0200

    Create 1406A-SubsetMex.py

---
+++ Python/1406A-SubsetMex.py
@@ -0,0 +1,28 @@
+#https://codeforces.com/problemset/problem/1406/A
+
+import sys
+input = sys.stdin.readline
+ins = lambda: input().rstrip()
+ini = lambda: int(input().rstrip())
+inm = lambda: map(int, input().rstrip().split())
+inl = lambda: list(map(int, input().split()))
+out = lambda x, s='\n': print(s.join(map(str, x)))
+
+t = ini()
+for _ in range(t):
+    n = ini()
+    a = sorted(inl())
+    first = second = -1
+    for i in range(101):
+        if first != -1 and second != -1:
+            break
+        if a.count(i) > 1:
+            continue
+        if a.count(i) == 1 and first == -1:
+            first = i
+            continue
+        if a.count(i) == 0:
+            first = i if first == -1 else first
+            second = i
+            break
+    print(first+second)
