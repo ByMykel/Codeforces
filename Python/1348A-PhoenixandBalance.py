43f1a655377a23db4b0c3d9f8286c775a0b65eb1
ByMykel <38622893+ByMykel@users.noreply.github.com>
2020-09-19 15:59:07 +0200

    Update 1348A-PhoenixandBalance.py

--- Python/1348A-PhoenixandBalance.py
+++ Python/1348A-PhoenixandBalance.py
@@ -11,4 +11,4 @@ out = lambda x, s='\n': print(s.join(map(str, x)))
 t = ini()
 for _ in range(t):
     n = ini()
-    print(2**n + sum([2**i for i in range(1, n//2)]) - sum(2**i for i in range(n//2, n)))
+    print(2**n + sum(2**i for i in range(1, n//2)) - sum(2**i for i in range(n//2, n)))
