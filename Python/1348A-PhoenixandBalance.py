dd6d341da107f7a1707e7968b6c346f5e3d6f0a5
ByMykel <38622893+ByMykel@users.noreply.github.com>
2020-09-19 15:58:27 +0200

    Create 1348A-PhoenixandBalance.py

---
+++ Python/1348A-PhoenixandBalance.py
@@ -0,0 +1,14 @@
+#https://codeforces.com/problemset/problem/1348/A
+
+import sys
+input = sys.stdin.readline
+ins = lambda: input().rstrip()
+ini = lambda: int(input().rstrip())
+inm = lambda: map(int, input().rstrip().split())
+inl = lambda: list(map(int, input().split()))
+out = lambda x, s='\n': print(s.join(map(str, x)))
+ 
+t = ini()
+for _ in range(t):
+    n = ini()
+    print(2**n + sum([2**i for i in range(1, n//2)]) - sum(2**i for i in range(n//2, n)))
