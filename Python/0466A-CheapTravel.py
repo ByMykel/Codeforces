a8a9111dc8f4e38bd2d6c2417199e70522c6e518
ByMykel <38622893+ByMykel@users.noreply.github.com>
2020-09-19 16:16:46 +0200

    Create 0466A-CheapTravel.py

---
+++ Python/0466A-CheapTravel.py
@@ -0,0 +1,12 @@
+#https://codeforces.com/problemset/problem/466/A
+
+import sys
+input = sys.stdin.readline
+ins = lambda: input().rstrip()
+ini = lambda: int(input().rstrip())
+inm = lambda: map(int, input().rstrip().split())
+inl = lambda: list(map(int, input().split()))
+out = lambda x, s='\n': print(s.join(map(str, x)))
+
+n, m, a, b = inm()
+print(min(n*a, (n+m-1) // m * b, n//m * b + (n - (n//m) * m) * a))
