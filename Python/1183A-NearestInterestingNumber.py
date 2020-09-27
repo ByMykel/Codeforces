948f3e3868472862b193aea310c34cb54439ff75
ByMykel <38622893+ByMykel@users.noreply.github.com>
2020-09-27 23:16:17 +0200

    Create 1183A-NearestInterestingNumber.py

---
+++ Python/1183A-NearestInterestingNumber.py
@@ -0,0 +1,15 @@
+#https://codeforces.com/problemset/problem/1183/A
+
+import sys
+input = sys.stdin.readline
+ins = lambda: input().rstrip()
+ini = lambda: int(input().rstrip())
+inm = lambda: map(int, input().rstrip().split())
+inl = lambda: list(map(int, input().split()))
+out = lambda x, s='\n': print(s.join(map(str, x)))
+
+n = ini()
+near = lambda n: sum(int(i) for i in str(n))
+while near(n) % 4 != 0:
+    n += 1
+print(n)
