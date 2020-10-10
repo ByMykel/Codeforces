9aa178a11b0249669559fcdd0b2aaeca4d3b6a11
ByMykel <38622893+ByMykel@users.noreply.github.com>
2020-10-10 17:07:52 +0200

    Create 1180A-AlexandaRhombus.py

---
+++ Python/1180A-AlexandaRhombus.py
@@ -0,0 +1,12 @@
+#https://codeforces.com/problemset/problem/1180/A
+
+import sys
+input = sys.stdin.readline
+ins = lambda: input().rstrip()
+ini = lambda: int(input().rstrip())
+inm = lambda: map(int, input().rstrip().split())
+inl = lambda: list(map(int, input().split()))
+out = lambda x, s='\n': print(s.join(map(str, x)))
+
+n = ini()
+print(n*n + (n-1)*(n-1))
