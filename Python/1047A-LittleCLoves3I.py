ff4a75dc0dba8bd711ee0f7bd570ddfbed256a40
ByMykel <38622893+ByMykel@users.noreply.github.com>
2020-10-11 02:20:02 +0200

    Create 1047A-LittleCLoves3I.py

---
+++ Python/1047A-LittleCLoves3I.py
@@ -0,0 +1,15 @@
+#https://codeforces.com/problemset/problem/1047/A
+
+import sys
+input = sys.stdin.readline
+ins = lambda: input().rstrip()
+ini = lambda: int(input().rstrip())
+inm = lambda: map(int, input().rstrip().split())
+inl = lambda: list(map(int, input().split()))
+out = lambda x, s='\n': print(s.join(map(str, x)))
+
+n = ini()
+if (n-2) % 3 != 0:
+    print(1, 1, n-2)
+else:
+    print(1, 2, n-3)
