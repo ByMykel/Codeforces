f7fd3df7b3e13809345f748e83857cb91db3efe9
ByMykel <38622893+ByMykel@users.noreply.github.com>
2020-10-11 19:19:13 +0200

    Create 1093A-DiceRolling.py

---
+++ Python/1093A-DiceRolling.py
@@ -0,0 +1,14 @@
+#https://codeforces.com/problemset/problem/1093/A
+
+import sys
+input = sys.stdin.readline
+ins = lambda: input().rstrip()
+ini = lambda: int(input().rstrip())
+inm = lambda: map(int, input().rstrip().split())
+inl = lambda: list(map(int, input().split()))
+out = lambda x, s='\n': print(s.join(map(str, x)))
+
+t = ini()
+for _ in range(t):
+    n = ini()
+    print((n + 6) // 7)
