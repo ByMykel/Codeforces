4a83cccc5e4b07e90d03f841c0165890d474ec3c
ByMykel <38622893+ByMykel@users.noreply.github.com>
2020-10-11 19:49:01 +0200

    Create 1430B-Barrels.py

---
+++ Python/1430B-Barrels.py
@@ -0,0 +1,15 @@
+#https://codeforces.com/contest/1430/problem/B
+
+import sys
+input = sys.stdin.readline
+ins = lambda: input().rstrip()
+ini = lambda: int(input().rstrip())
+inm = lambda: map(int, input().rstrip().split())
+inl = lambda: list(map(int, input().split()))
+out = lambda x, s='\n': print(s.join(map(str, x)))
+
+t = ini()
+for _ in range(t):
+    n, k = inm()
+    a = sorted(inl())
+    print(sum(a[-k-1:]))
