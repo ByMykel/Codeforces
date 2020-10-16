0597636ec3ea3820ec793b2985a409b6c0cbc127
ByMykel <38622893+ByMykel@users.noreply.github.com>
2020-10-16 22:08:34 +0200

    Create 0918A-Eleven.py

---
+++ Python/0918A-Eleven.py
@@ -0,0 +1,17 @@
+#https://codeforces.com/problemset/problem/918/A
+
+import sys
+input = sys.stdin.readline
+ins = lambda: input().rstrip()
+ini = lambda: int(input().rstrip())
+inm = lambda: map(int, input().rstrip().split())
+inl = lambda: list(map(int, input().split()))
+out = lambda x, s='\n': print(s.join(map(str, x)))
+
+n = ini()
+ans = ["o"] * n
+a = b = 1
+while b <= n:
+    ans[b-1] = ans[b-1].upper()
+    a, b = b, a + b
+out(ans, "")
