526cdad6f08bbd66deb98745775df03e2712d449
ByMykel <38622893+ByMykel@users.noreply.github.com>
2020-10-17 21:05:00 +0200

    Create 1104A-Splittingintodigits.py

---
+++ Python/1104A-Splittingintodigits.py
@@ -0,0 +1,13 @@
+#https://codeforces.com/problemset/problem/1104/A
+
+import sys
+input = sys.stdin.readline
+ins = lambda: input().rstrip()
+ini = lambda: int(input().rstrip())
+inm = lambda: map(int, input().rstrip().split())
+inl = lambda: list(map(int, input().split()))
+out = lambda x, s='\n': print(s.join(map(str, x)))
+
+n = ini()
+print(n)
+out([1] * n, " ")
