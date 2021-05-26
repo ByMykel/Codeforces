fc801855710ef5d3e824c5742f853cb0923d9e93
ByMykel <38622893+ByMykel@users.noreply.github.com>
2021-05-26 20:31:38 +0200

    Create 0932A-PalindromicSupersequence.py

---
+++ Python/0932A-PalindromicSupersequence.py
@@ -0,0 +1,12 @@
+#https://codeforces.com/problemset/problem/932/A
+
+import sys
+input = sys.stdin.readline
+ins = lambda: input().rstrip()
+ini = lambda: int(input().rstrip())
+inm = lambda: map(int, input().rstrip().split())
+inl = lambda: list(map(int, input().split()))
+out = lambda x, s='\n': print(s.join(map(str, x)))
+
+s = ins()
+print(s + s[::-1])
