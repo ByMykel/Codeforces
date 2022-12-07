bb507305d694a2153dfb8d86822bf8a7bef09658
ByMykel <38622893+ByMykel@users.noreply.github.com>
2022-12-07 20:08:55 +0100

    Create 1760B-Atilla'sFavoriteProblem.py

---
+++ Python/1760B-Atilla'sFavoriteProblem.py
@@ -0,0 +1,16 @@
+# https://codeforces.com/contest/1760/problem/B
+
+import sys
+input = sys.stdin.readline
+ins = lambda: input().rstrip()
+ini = lambda: int(input().rstrip())
+inm = lambda: map(int, input().rstrip().split())
+inl = lambda: list(map(int, input().split()))
+out = lambda x, s='\n': print(s.join(map(str, x)))
+
+t = ini()
+for _ in range(t):
+    n = ini()
+    s = ins()
+    last = sorted(list(s))[-1]
+    print(ord(last) - 96)
